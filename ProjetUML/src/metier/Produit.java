1ad2be784866a7b7389ad3b86428ba0f1523a178
dibartolomeop <dibartolomeop@K116-5.dep-info.iutmontp.univ-montp2.fr>
2016-12-07 16:18:36 +0100

    coucou

---
+++ ProjetUML/src/metier/Produit.java
@@ -0,0 +1,57 @@
+package metier;
+
+
+public class Produit implements I_Produit{
+
+	private int quantiteStock;
+	private String nom;
+	private double prixUnitaireHT;
+	private static double tauxTVA = 0.2;
+	
+	public Produit(String nom, double prixUnitaireHT,int quantiteStock) {
+		this.quantiteStock = quantiteStock;
+		this.nom = nom;
+		this.prixUnitaireHT = prixUnitaireHT;
+	}
+	
+	@Override
+	public boolean ajouter(int qteAchetee) {
+		quantiteStock += qteAchetee;
+		return true;
+	}
+
+	@Override
+	public boolean enlever(int qteVendue) {
+		if(qteVendue <= quantiteStock){
+			quantiteStock-=qteVendue;
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public String getNom() {
+		return nom;
+	}
+
+	@Override
+	public int getQuantite() {
+		return quantiteStock;
+	}
+
+	@Override
+	public double getPrixUnitaireHT() {
+		return prixUnitaireHT*100 /100;
+	}
+
+	@Override
+	public double getPrixUnitaireTTC() {
+		return prixUnitaireHT*(1+tauxTVA)*100 /100;
+	}
+
+	@Override
+	public double getPrixStockTTC() {
+		return quantiteStock*getPrixUnitaireTTC()*100 /100;
+	}
+
+}
